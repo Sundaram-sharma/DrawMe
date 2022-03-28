9b704e523cdaa7834f1e2ee00c1be875da3c336b
sundaramsharma <sundaramsharma1209@gmail.com>
2022-03-27 21:08:38 -0400

    updated functionality

--- app/src/main/java/com/example/drawme/DrawingView.kt
+++ app/src/main/java/com/example/drawme/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.drawme
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
@@ -28,6 +29,37 @@ class DrawingView(context: Context, attr: AttributeSet): View(context, attr) {
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)//bitmap configuration
+        canvas = Canvas(mCanvasBitmap!!) // set canvas as mCanvasBitmap
+
+    }
+        // Canvas? to canvas if fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f,mCanvasPaint)
+
+            if(!mDrawPath!!.isEmpty) {
+                mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness //set the brush thickness
+                mDrawPaint!!.color = mDrawPath!!.color //set the color
+                canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+            }
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean { //what happen when we touch
+
+        val touchx = event?.x
+        val touchy = event?.y
+
+        when (event?.action){
+
+        }
+
+        return super.onTouchEvent(event)
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path(){
